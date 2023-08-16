ac1963b5049a882ca29e8faf48ea9582fe005262
Bayyeee <muhammadubai8@gmail.com>
2023-08-16 18:52:20 +0800

    menambahkan folder cetak,opsi,spesifikasi : (masih ada yang kurang)

---
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/cetak/CetakstikerActivity.kt
@@ -0,0 +1,12 @@
+package umbjm.ft.inf.mydigitalprinting.produk.cetak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import umbjm.ft.inf.mydigitalprinting.R
+
+class CetakstikerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cetakstiker)
+    }
+}
---
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/opsi/OpsiundanganActivity.kt
@@ -0,0 +1,12 @@
+package umbjm.ft.inf.mydigitalprinting.produk.opsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import umbjm.ft.inf.mydigitalprinting.R
+
+class OpsiundanganActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opsi_undangan)
+    }
+}
