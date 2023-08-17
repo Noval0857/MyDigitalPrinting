80651799cec6f6ea05c03c9cda193dd2f909db9f
Bayyeee <muhammadubai8@gmail.com>
2023-08-17 10:52:37 +0800

    Pekerjaan Latifah

---
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/login/ChangepasswordActivity.kt
@@ -0,0 +1,12 @@
+package umbjm.ft.inf.mydigitalprinting.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import umbjm.ft.inf.mydigitalprinting.R
+
+class ChangepasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
