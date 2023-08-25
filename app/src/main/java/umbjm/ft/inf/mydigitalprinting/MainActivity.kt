ec9abcf8804e6dd2e5005a7fc8407dc893e5393f
Bayyeee <muhammadubai8@gmail.com>
2023-08-25 18:22:50 +0800

    Konfigurasi Log Out di halaman profile

--- app/src/main/java/umbjm/ft/inf/mydigitalprinting/MainActivity.kt
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/MainActivity.kt
@@ -1,14 +1,20 @@
 package umbjm.ft.inf.mydigitalprinting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
+import umbjm.ft.inf.mydigitalprinting.databinding.ActivityMainBinding
 import umbjm.ft.inf.mydigitalprinting.item.GridAdapter
 import umbjm.ft.inf.mydigitalprinting.item.GridItem
+import umbjm.ft.inf.mydigitalprinting.profil.ProfileActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
@@ -19,15 +25,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ActionBar agar dapat di konfigurasi di halaman Activity_Main
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Mengambil fungsi init
         init()
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
-        val navigationView: NavigationView = findViewById(R.id.navigationView)
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+    }
+
+    // Untuk menampilkan Toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Fungsi toolbar jika diclick
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.keranjang -> {
+            // Konfigurasikan jika ingin pindah halaman
+            }
+
+            R.id.Tolprofile -> {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+            R.id.Tolsetting -> {
+            // Konfigurasikan jika ingin pindah halaman
+
+            }
+
+            R.id.Tolpesanan -> {
+            // Konfigurasikan jika ingin pindah halaman
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun init() {
--- app/src/main/java/umbjm/ft/inf/mydigitalprinting/profil/ProfileActivity.kt
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/profil/ProfileActivity.kt
@@ -1,12 +1,38 @@
 package umbjm.ft.inf.mydigitalprinting.profil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import umbjm.ft.inf.mydigitalprinting.R
+import umbjm.ft.inf.mydigitalprinting.databinding.ActivityProfilBinding
+import umbjm.ft.inf.mydigitalprinting.login.LoginActivity
 
 class ProfileActivity : AppCompatActivity() {
+
+    // Untuk mengakses Library
+    lateinit var binding : ActivityProfilBinding
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityProfilBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profil)
+        setContentView(binding.root)
+
+        // Memberikan Aksi ke btnkeluar
+        binding.btnKeluar.setOnClickListener {
+            // Mengambil fungsi btnlogout dan menampungnya ke btnkeluar
+            btnLogout()
+        }
+    }
+
+    // Memberikan fungsi ke btnlogout
+    private fun btnLogout() {
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+        Toast.makeText(this, "Anda berhasil Log Out", Toast.LENGTH_SHORT).show()
     }
 }
