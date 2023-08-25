ed79018299d751f46e6792c85a35dc9325ace7a6
Muhammad Noval <echo.muhammadnoval@gmail.com>
2023-08-25 09:51:02 +0700

    konfigurasi pembayaran

--- app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecstickerActivity.kt
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecstickerActivity.kt
@@ -1,12 +1,24 @@
 package umbjm.ft.inf.mydigitalprinting.produk.spesifikasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 import umbjm.ft.inf.mydigitalprinting.R
+import umbjm.ft.inf.mydigitalprinting.produk.PembayaranActivity
 
 class SpecstickerActivity : AppCompatActivity() {
+
+    private lateinit var bayar:MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spesifikasistiker)
+
+        bayar = findViewById(R.id.btnS1)
+
+        bayar.setOnClickListener {
+            val intent = Intent(this, PembayaranActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecundanganActivity.kt
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecundanganActivity.kt
@@ -1,12 +1,24 @@
 package umbjm.ft.inf.mydigitalprinting.produk.spesifikasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 import umbjm.ft.inf.mydigitalprinting.R
+import umbjm.ft.inf.mydigitalprinting.produk.PembayaranActivity
 
 class SpecundanganActivity : AppCompatActivity() {
+
+    private lateinit var bayar:MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spesifikasiundangan)
+
+        bayar = findViewById(R.id.btnS1)
+
+        bayar.setOnClickListener {
+            val intent = Intent(this, PembayaranActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
