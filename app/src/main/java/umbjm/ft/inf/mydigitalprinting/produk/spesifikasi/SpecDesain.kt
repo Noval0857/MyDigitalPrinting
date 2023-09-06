097697b8610a4488071ce7d44a9ec75a0f7673ba
Muhammad Noval <echo.muhammadnoval@gmail.com>
2023-09-06 08:55:38 +0700

    banyak

---
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecDesain.kt
@@ -0,0 +1,13 @@
+package umbjm.ft.inf.mydigitalprinting.produk.spesifikasi
+
+data class SpecDesain(
+    val idProduk: String?= null,
+    val jenis: String?= null,
+    val teksUtama: String?= null,
+    val teksLainnya: String?= null,
+    val keterangan: String? = null,
+    val panjang: String? = null,
+    val lebar: String? = null,
+    val image: String? = ""
+)
+
--- app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecdesainActivity.kt
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/spesifikasi/SpecdesainActivity.kt
@@ -1,79 +1,180 @@
 package umbjm.ft.inf.mydigitalprinting.produk.spesifikasi
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.Toast
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import umbjm.ft.inf.mydigitalprinting.MainActivity
-import umbjm.ft.inf.mydigitalprinting.R
-import umbjm.ft.inf.mydigitalprinting.login.RegistrasiActivity
+import umbjm.ft.inf.mydigitalprinting.databinding.ActivitySpesifikasidesainBinding
 
-class SpecdesainActivity : AppCompatActivity() {
 
-    private lateinit var bayar: MaterialButton
-    private lateinit var jenis_spanduk: TextInputEditText
-    private lateinit var teks_utama: TextInputEditText
-    private lateinit var teks_lainnya: TextInputEditText
-    private lateinit var keterangan: TextInputEditText
-    private lateinit var panjang: TextInputEditText
-    private lateinit var lebar: TextInputEditText
-    private lateinit var image: ImageView
-
-    //    private var selectedImageUri: Uri? = null
+class SpecdesainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivitySpesifikasidesainBinding
     private lateinit var database: DatabaseReference
-
+    private var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_spesifikasidesain)
+        binding = ActivitySpesifikasidesainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bayar = findViewById(R.id.btnS1)
-        jenis_spanduk = findViewById(R.id.jenis)
-        teks_utama = findViewById(R.id.teks_utama)
-        teks_lainnya = findViewById(R.id.teks_lainnya)
-        keterangan = findViewById(R.id.keterangan)
-        panjang = findViewById(R.id.panjang)
-        lebar = findViewById(R.id.lebar)
-        image = findViewById(R.id.image)
+        imageInput()
+        upload()
 
-        database = FirebaseDatabase.getInstance().getReference("SpecDesain")
+    }
 
-        bayar.setOnClickListener {
-            Pesan()
+    private fun imageInput(){
+        binding.image.setOnClickListener {
+            resultLauncher.launch("image/*")
         }
     }
 
-    private fun Pesan() {
-        val jenisSpanduk = jenis_spanduk.text.toString()
-        val teksUtama = teks_utama.text.toString()
-        val teksLainnya = teks_lainnya.text.toString()
-        val Keterangan = keterangan.text.toString()
-        val Panjang = panjang.text.toString()
-        val Lebar = lebar.text.toString()
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) {
+
+        imageUri = it
+        binding.image.setImageURI(it)
+    }
 
 
-        val sdesainId = database.push().key.toString()
-        val SDesain = specDesain(jenisSpanduk, teksUtama, teksLainnya, Keterangan, Panjang, Lebar)
-        database.child(sdesainId).setValue(SDesain).addOnCompleteListener {
-            jenis_spanduk.setText("")
-            teks_utama.setText("")
-            teks_lainnya.setText("")
-            keterangan.setText("")
-            panjang.setText("")
-            lebar.setText("")
-            Toast.makeText(this, "Berhasil", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }.addOnFailureListener {
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+    private fun upload(){
+        binding.btnS1.setOnClickListener {
+            val jenis = binding.UploadJenis.text.toString()
+            val teksUtama = binding.UploadteksUtama.text.toString()
+            val teksLainnya = binding.UploadteksLainnya.text.toString()
+            val keterangan = binding.Uploadketerangan.text.toString()
+            val panjang = binding.Uploadpanjang.text.toString()
+            val lebar = binding.Uploadlebar.text.toString()
+
+
+            // Referensi Firebase Storage
+            val storageRef = FirebaseStorage.getInstance().reference.child("Images").child(System.currentTimeMillis().toString())
+
+            // Mengunggah gambar ke Firebase Storage
+            imageUri?.let {
+                storageRef.putFile(it).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Jika pengunggahan berhasil, dapatkan URL gambar
+                        storageRef.downloadUrl.addOnSuccessListener { uri ->
+                            // Simpan URL gambar ke Firebase Realtime Database
+                            val imageUrl = uri.toString()
+                            database = FirebaseDatabase.getInstance("https://mydigitalprinting-60323-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Produk")
+                            val idProduk = database.push().key!!
+                            val sD = SpecDesain(idProduk, jenis, teksUtama, teksLainnya, keterangan, panjang, lebar, imageUrl)
+                            database.child(idProduk).setValue(sD).addOnCompleteListener { databaseTask ->
+                                if (databaseTask.isSuccessful) {
+                                    Toast.makeText(this, "Uploaded Successfully", Toast.LENGTH_SHORT).show()
+                                    val intent = Intent(this, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                } else {
+                                    Toast.makeText(this, "Gagal", Toast.LENGTH_SHORT).show()
+                                }
+                                binding.UploadJenis.text?.clear()
+                                binding.UploadteksUtama.text?.clear()
+                                binding.UploadteksLainnya.text?.clear()
+                                binding.Uploadketerangan.text?.clear()
+                                binding.Uploadpanjang.text?.clear()
+                                binding.Uploadlebar.text?.clear()
+
+                            }
+                        }
+                    } else {
+                        Toast.makeText(this, "Gagal", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
+
+//    private val ActivityResultLauncher = registerForActivityResult<Intent, ActivityResult>(
+//        ActivityResultContracts.StartActivityForResult()
+//    ){result: ActivityResult ->
+//        if (result.resultCode== RESULT_OK){
+//            val uri = result.data!!.data
+//            try {
+//                val inputStream = contentResolver.openInputStream(uri!!)
+//                val myBitmap = BitmapFactory.decodeStream(inputStream)
+//                val stream = ByteArrayOutputStream()
+//                myBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+//                val bytes = stream.toByteArray()
+//
+//                binding.image.setImageBitmap(myBitmap)
+//                inputStream!!.close()
+//            } catch (ex: Exception){
+//                Toast.makeText(this, ex.message.toString(), Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 }
+
+//class SpecdesainActivity : AppCompatActivity() {
+//
+//    private lateinit var bayar: MaterialButton
+//    private lateinit var jenis_spanduk: TextInputEditText
+//    private lateinit var teks_utama: TextInputEditText
+//    private lateinit var teks_lainnya: TextInputEditText
+//    private lateinit var keterangan: TextInputEditText
+//    private lateinit var panjang: TextInputEditText
+//    private lateinit var lebar: TextInputEditText
+//    private lateinit var image: ImageView
+//
+//    //    private var selectedImageUri: Uri? = null
+//    private lateinit var database: DatabaseReference
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_spesifikasidesain)
+//
+//        bayar = findViewById(R.id.btnS1)
+//        jenis_spanduk = findViewById(R.id.jenis)
+//        teks_utama = findViewById(R.id.teks_utama)
+//        teks_lainnya = findViewById(R.id.teks_lainnya)
+//        keterangan = findViewById(R.id.keterangan)
+//        panjang = findViewById(R.id.panjang)
+//        lebar = findViewById(R.id.lebar)
+//        image = findViewById(R.id.image)
+//
+//        database = FirebaseDatabase.getInstance().getReference("SpecDesain")
+//
+//        bayar.setOnClickListener {
+//            Pesan()
+//        }
+//    }
+//
+//    private fun Pesan() {
+//        val jenisSpanduk = jenis_spanduk.text.toString()
+//        val teksUtama = teks_utama.text.toString()
+//        val teksLainnya = teks_lainnya.text.toString()
+//        val Keterangan = keterangan.text.toString()
+//        val Panjang = panjang.text.toString()
+//        val Lebar = lebar.text.toString()
+//
+//
+//        val sdesainId = database.push().key.toString()
+//        val SDesain = specDesain(jenisSpanduk, teksUtama, teksLainnya, Keterangan, Panjang, Lebar)
+//        database.child(sdesainId).setValue(SDesain).addOnCompleteListener {
+//            jenis_spanduk.setText("")
+//            teks_utama.setText("")
+//            teks_lainnya.setText("")
+//            keterangan.setText("")
+//            panjang.setText("")
+//            lebar.setText("")
+//            Toast.makeText(this, "Berhasil", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }.addOnFailureListener {
+//            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//}
 //    private fun openFile() {
 //        val intent = Intent(Intent.ACTION_GET_CONTENT)
 //        intent.type = "image/*"
