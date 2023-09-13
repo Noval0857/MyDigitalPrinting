f54a87c342d2832fc6fd46fd7f45a1a6ddb7c4fa
Bayyeee <muhammadubai8@gmail.com>
2023-09-13 23:40:10 +0800

    PerbaikanXML3

--- app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/cetak/CetakdesainActivity.kt
+++ app/src/main/java/umbjm/ft/inf/mydigitalprinting/produk/cetak/CetakdesainActivity.kt
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.button.MaterialButton
 import umbjm.ft.inf.mydigitalprinting.R
+import umbjm.ft.inf.mydigitalprinting.produk.opsi.OpsipesananActivity
 
 class CetakdesainActivity : AppCompatActivity() {
 
-    private lateinit var bayar:MaterialButton
+    private lateinit var pesan:MaterialButton
+    private lateinit var btnKembali:MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cetak)
 
-        bayar = findViewById(R.id.btnS1)
+        btnKembali = findViewById(R.id.btnKembali)
+        btnKembali.setOnClickListener {
+            val intent = Intent(this, OpsipesananActivity::class.java)
+            startActivity(intent)
+            return@setOnClickListener
+        }
+
+        pesan = findViewById(R.id.btnPesan)
 
 //        bayar.setOnClickListener {
 //            val intent = Intent(this, PembayaranActivity::class.java)
